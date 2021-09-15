ffdedb3b084b11f2762dcb4604849f8b6c07c4c9
Dan Soup <dansoup@hotmail.co.uk>
2021-09-15 21:16:45 +0100

    webpack bundle issues

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname , 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    filename: 'index_bundle.js'
   },
   module: {
     rules: [
